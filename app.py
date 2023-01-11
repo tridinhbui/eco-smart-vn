69fe7deda89af1e68dde7e631c99ac4bbd7f3b78
minh-nguyen-mqn <minhnguyen.mqn@gmail.com>
2023-01-12 01:51:49 +0700

    Project setup retry 4

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from flask import Flask, redirect, url_for, render_template
+from flask_flatpages import FlatPages
+
+# Tell Flatpages to auto reload when a page is changed, and look for .md files
+FLATPAGES_AUTO_RELOAD = True
+FLATPAGES_EXTENSION = '.md'
+
+# Create our app object, use this page as our settings (will pick up DEBUG)
 app = Flask(__name__)
+
+# For settings, we just use this file itself, very easy to configure
+app.config.from_object(__name__)
+
+# We want Flask to allow no slashes after paths, because they get turned into flat files
+app.url_map.strict_slashes = False
+
+# Create an instance of our extension
+pages = FlatPages(app)
 
 @app.route('/')
 def index():
