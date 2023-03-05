3c4310bf7409015d44a57ab1288696f5018b2754
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2023-03-05 21:29:56 +0700

    Add Process Not completely responsive and JS error

---
+++ project/static/js/process.js
@@ -0,0 +1,23 @@
+$(".st1").click(function() {
+    $(".step1").prop("checked", true);
+})
+
+$(".st2").click(function() {
+    $(".step2").prop("checked", true);
+})
+
+$(".st3").click(function() {
+    $(".step3").prop("checked", true);
+})
+
+$(".st4").click(function() {
+    $(".step4").prop("checked", true);
+})
+
+$(".st5").click(function() {
+    $(".step5").prop("checked", true);
+})
+
+$(".st6").click(function() {
+    $(".step6").prop("checked", true);
+})
