a203f4efd0d05cd4632b2bb87c8805ced4d08ae6
hoangph.aime <hoangph.aime@gmail.com>
2023-03-24 21:02:32 -0500

    update redis

--- config/account.js
+++ config/account.js
@@ -5,4 +5,5 @@ export const DEFAULT_MAIL = 'hoangph7298@gmail.com';
 export const TINYMCE_KEY = '81fdyoznezq2x6v6ijlrpwayx6eb5wcgzeplnniti3w5eadu';
 export const CLOUD_NAME = 'douigqvec';
 export const UPLOAD_PRESET_NAME = 'ebue3uor';
-export const REDIS_URL ='redis://default:54c38d933c93467497685a41f45606a4@us1-arriving-grouse-40395.upstash.io:40395';
+export const REDIS_URL ='https://us1-arriving-grouse-40395.upstash.io';
+export const PASSWORD = 'AZ3LACQgNjBkZjRkMzMtMzgxOS00MTNlLWE2MGUtMDM5MTlmODgxYjFlNTRjMzhkOTMzYzkzNDY3NDk3Njg1YTQxZjQ1NjA2YTQ=';
