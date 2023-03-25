466235f881bf4256bff3853c52e38244d2cef07a
GiangNPham <giangpn25@gmail.com>
2023-03-25 18:20:47 +0700

    animation left to right for product feature

--- project/static/js/productfeature.js
+++ project/static/js/productfeature.js
@@ -1,8 +1,8 @@
-window.addEventListener('scroll', function() {
-    var target = document.querySelector('#container-product');
-    var targetPos = target.getBoundingClientRect().top + window.pageYOffset;
-    var scrollPos = window.scrollY + window.innerHeight;
-    if (scrollPos >= targetPos) {
-      document.querySelector('.productfeature_loading').classList.add('show');
-    }
-  });
+window.addEventListener('scroll', function () {
+  var target = document.querySelector('#container-product');
+  var targetPos = target.getBoundingClientRect().top + window.pageYOffset;
+  var scrollPos = window.scrollY + window.innerHeight;
+  if (scrollPos >= targetPos) {
+    document.querySelector('.productfeature_loading').classList.add('show');
+  }
+});
